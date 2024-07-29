ac4b7cd84d0bf134b87940e749530d133d6dbddc
heroleggo <h3r0.y0u@gmail.com>
2024-07-29 22:12:50 +0900

    feat: add functions on exercise package
    - add many format of functions in go language

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ---
 
 So, I decided to use golang to make a simple investment bot.
+
+
+---
+
+## Tips
+
+Tip 1. Go's function parameter is call by value. call by reference is not used.
--- src/exercise/exercise.go
+++ src/exercise/exercise.go
@@ -5,3 +5,24 @@ import "fmt"
 func SayHello() {
 	fmt.Println("Hello")
 }
+
+func functionName(a int, b string) int {
+	return a
+}
+
+func functionName2(a int, b string) string {
+	return b
+}
+
+func functionName3(a int, b int) int {
+	return a + b
+}
+
+func MultiReturnFunc(a int, b int) (int, string) {
+	return a + b, "hehe"
+}
+
+func FuncWithResultValueName(a int) (result int) {
+	result = a
+	return
+}
