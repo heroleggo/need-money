b629165047196cd8495cde1c304672593f984c46
heroleggo <h3r0.y0u@gmail.com>
2024-08-08 20:42:53 +0900

    feat: add method on books

--- src/exercise/library.go
+++ src/exercise/library.go
@@ -21,3 +21,49 @@ const (
 )
 
 // 가지고 있는 기능 : 책 등록하기, 책 조회하기(검색), 책 삭제하기, 책 대여하기, 책 반납하기
+
+func (b *Books) AddBook(book Book) {
+	*b = append(*b, book)
+}
+
+func (b *Books) SearchBookByName(name string) (Book, bool) {
+	for _, book := range *b {
+		if book.Name == name {
+			return book, true
+		}
+	}
+	return Book{}, false
+}
+
+func (b *Books) SearchBookByIndex(index int) (Book, bool) {
+	for _, book := range *b {
+		if book.Index == index {
+			return book, true
+		}
+	}
+	return Book{}, false
+}
+
+func (b *Books) DeleteBook(index int) {
+	for i, book := range *b {
+		if book.Index == index {
+			*b = append((*b)[:i], (*b)[i+1:]...)
+		}
+	}
+}
+
+func (b *Books) RentBook(index int) {
+	for i, book := range *b {
+		if book.Index == index {
+			(*b)[i].Status = true
+		}
+	}
+}
+
+func (b *Books) ReturnBook(index int) {
+	for i, book := range *b {
+		if book.Index == index {
+			(*b)[i].Status = false
+		}
+	}
+}
